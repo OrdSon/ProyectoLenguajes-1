e0b03d16e4fe0b8d12dd61c76f078b6401c8606b
OrdSon <68841451+OrdSon@users.noreply.github.com>
2020-09-18 22:24:19 -0600

    Casi....

--- ProyectoLenguaje/ProyectoLenguaje/Analizador.cs
+++ ProyectoLenguaje/ProyectoLenguaje/Analizador.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,62 +15,142 @@ namespace ProyectoLenguaje
 {
     public class Analizador
     {
-        private LinkedList<Token> salida;
-        private int estado;
-        String auxiliar;
-        public void analizar(RichTextBox textBox)
-        {
-            String texto = textBox.Text;
-            String exp1= "00021310321 ++++++++++++ + ++";
-            String exp2 = "123120123";
-            String expresionCadenas = "^\"[^\"]*\"$";
-            String expresionNumeros = "^(0|([1-9]+[0-9]*))$";
-            String expresionCharts = "'.?'";
-            String expresionAritmetica = "^(\\+\\+?)$|^(\\-\\-?)$|^(\\*)$|^(\\/)$";
-            String expresionRelacional = "^(\\<|\\>)\\=?$|^(\\=|\\!)\\=$";
-            String expresionLogica = "^(\\|){1,2}$|^(\\&){1,2}$|^(\\!)$";
-            String expresionIdentificador = "^[a-zA-Z1-9-_]*$";
 
-            Regex regex = new Regex(expresionIdentificador);
-            MatchCollection match = regex.Matches(texto);
-            foreach(Match e in match){
-                if (match.Count > 0)
-                {
-                    textBox.Select(e.Index,e.Length);
-                    textBox.SelectionColor = Color.Red;
-                }
-            }
+        LinkedList<Token> tokens;
+        Regex regexCadenas = new Regex("\"([^\"]*)\" | '([^']*)'");
+        Regex regexEnteros = new Regex("^(0|([1-9]+[0-9]*))$");
+        Regex regexBoolean = new Regex("^(true)$|^(false)$");
+        Regex regexDecimal = new Regex("^(0|([1-9]+[0-9]*)\\.[0-9]+)$");
+        Regex regexChart = new Regex("(\'.?\'){1}");
+        Regex regexAritmetica = new Regex("^(\\+\\+?)$|^(\\-\\-?)$|^(\\*)$|^(\\/)$");
+        Regex regexRelacional = new Regex("^(\\<|\\>)\\=?$|^(\\=|\\!)\\=$");
+        Regex regexLogica = new Regex("^(\\|){1,2}$|^(\\&){1,2}$|^(\\!)$");
+        Regex regexIdentificador = new Regex("^[a-zA-Z1-9-_]*$");
+        Regex regexReservadas = new Regex("^(SI)(NO)?(_SI)?$|^(MIENTRAS)$|^(HACER)$|^(PARA)$|^(HASTA)$|^(DESDE)$|^(INCREMENTO)$");
+        Regex regexTipoObjeto = new Regex("^(Entero)$|^(Decimal)$|^(Cadena)$|^(Boolean)$|^(Chart)$");
+
+        public void colorear (TextBox textBox)
+        {
+            MatchCollection cadenasEncontradas = regexCadenas.Matches(textBox.Text);
+            MatchCollection enterosEncontrados = regexEnteros.Matches(textBox.Text);
+            MatchCollection booleanEncontradas = regexBoolean.Matches(textBox.Text);
+            MatchCollection decimalesEncontradas = regexDecimal.Matches(textBox.Text);
+            MatchCollection chartsEncontradas = regexChart.Matches(textBox.Text);
+            MatchCollection aritmeticasEncontradas = regexAritmetica.Matches(textBox.Text);
+            MatchCollection relacionalEncontradas = regexRelacional.Matches(textBox.Text);
+            MatchCollection logicaEncontradas = regexLogica.Matches(textBox.Text);
+            MatchCollection identificadoresEncontrados = regexIdentificador.Matches(textBox.Text);
+            MatchCollection reservadasEncontradas = regexReservadas.Matches(textBox.Text);
+            MatchCollection tiposEncontrados = regexTipoObjeto.Matches(textBox.Text);
+           
         }
-        public LinkedList<Token> analizador(String entrada)
-        {
-            String prueba = "0009";
-            String reprueba = "123412124000012301";
-            entrada = entrada + "#";
-            salida = new LinkedList<Token>();
-            estado = 0;
-            auxiliar = "";
-            Char caracter;
-            for (int i = 0; i < entrada.Length; i++)
+        public void analizar(RichTextBox textBox)
             {
-                caracter = entrada.ElementAt(i);
-                switch (estado)
-                {
-                    case 0:
-
-                        break;
-                    case 1:
-
-                        break;
-                    case 2:
-
-                        break;
-                    case 3:
-
-                        break;
-                }
-
+            tokens = new LinkedList<Token>();
+            Token.Tipo tipo = new Token.Tipo();
+            String[] separadores = {" ", "\\s+",";", "\t" };
+            if (textBox.Lines.Length <= 0)
+            {
+                MessageBox.Show("Sin texto que analizar");
+                return;
             }
-            return null;
+            for(int i = 0; i < textBox.Lines.Length; i++)
+            {
+                String [] linea = textBox.Lines[i].Split(separadores, System.StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < linea.Length; j++)
+                {
+                    String valor = linea[j];
+                     if (regexDecimal.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.VALOR_DECIMAL;
+                        colorear(valor, textBox, Color.SkyBlue);
+                    }
+                    else if (regexEnteros.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.VALOR_ENTERO;
+                        colorear(valor, textBox, Color.Yellow);
+                    }
+                    else if (regexBoolean.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.VALOR_BOOLEAN;
+                        colorear(valor, textBox, Color.Orange);
+                    }
+                    else if (regexAritmetica.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.OPERADOR_ARITMETICO;
+                        colorear(valor, textBox, Color.Blue);
+                    }
+                    else if (regexRelacional.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.OPERADOR_RELACIONAL;
+                        colorear(valor, textBox, Color.Blue);
+                    }
+                    else if (regexLogica.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.OPERADOR_LOGICO;
+                        colorear(valor, textBox, Color.Blue);
+                    }
+                    else if (regexTipoObjeto.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.TIPO_OBJETO;
+                        colorear(valor, textBox, Color.DarkBlue);
+                    }
+                    else if (regexReservadas.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.PALABRA_RESERVADA;
+                        colorear(valor, textBox, Color.Green);
+                    }
+                    else if (regexIdentificador.IsMatch(valor))
+                    {
+                        tipo = Token.Tipo.IDENTIFICADOR;
+                        colorear(valor, textBox, Color.DarkGray);
+                    }
+                    
+                    else
+                    {
+                        tipo = Token.Tipo.ERROR;
+                    }
+                    tokens.AddLast(new Token(tipo, valor, (i+1)));
+                }
+            }
+
+            MatchCollection cadenasEncontradas = regexCadenas.Matches(textBox.Text);
+            foreach(Match e in cadenasEncontradas)
+            {
+                tokens.AddLast(new Token(Token.Tipo.VALOR_CADENA, e.Value, e.Index));
+                textBox.Select(e.Index, e.Length);
+                textBox.SelectionColor = Color.Purple;
+            }
+            MatchCollection charsEncontrados = regexChart.Matches(textBox.Text);
+            foreach (Match e in charsEncontrados)
+            {
+                tokens.AddLast(new Token(Token.Tipo.VALOR_CHART, e.Value, e.Index));
+                textBox.Select(e.Index, e.Length);
+                textBox.SelectionColor = Color.Yellow;
+            }
+
+        }
+        private void colorear(String valor, RichTextBox textBox, Color color)
+        {
+            if(valor == null)
+            {
+                return;
+            }
+            if (valor == "+" | valor == "-" | valor == "/" | valor == "*" | valor == "++" | valor == "--")
+            {
+                valor = "\\" + valor;
+            }
+            Regex regex = new Regex(valor);
+            MatchCollection encontrados = regex.Matches(textBox.Text);
+            if (!regex.IsMatch(textBox.Text))
+            {
+                return;
+            }
+            foreach (Match e in encontrados)
+            {
+                textBox.Select(e.Index, valor.Length);
+                textBox.SelectionColor = color;
+            }
         }
     }
 }
--- ProyectoLenguaje/ProyectoLenguaje/Token.cs
+++ ProyectoLenguaje/ProyectoLenguaje/Token.cs
@@ -11,57 +11,36 @@ namespace ProyectoLenguaje
     {
         public enum Tipo
         {
-            ENTERO,
-            DECIMAL,
-            CADENA,
-            BOOLEAN,
-            CHART,
-            IDENTIFICADOR_ENTERO,
-            IDENTIFICADOR_DECIMAL,
-            IDENTIFICADOR_CADENA,
-            IDENTIFICADOR_BOOLEAN,
-            IDENTIFICADOR_CHART,
-            OPERADOR_MAS,
-            OPERADOR_MENOS,
-            OPERADOR_MAS_MAS,
-            OPERADOR_MENOS_MENOS,
-            OPERADOR_POR,
-            OPERADOR_SLICE,
-            OPERADOR_MAYOR,
-            OPERADOR_MENOR,
-            OPERADOR_MAYOR_IGUAL,
-            OPERADOR_MENOR_IGUAL,
-            OPERADOR_IGUAL_IGUAL,
-            OPERADOR_NO_IGUAL,
-            OPERADOR_OR,
-            OPERADOR_AND,
-            OPERADOR_NO,
-            PARENTESIS_APERTURA,
-            PARENTESIS_CIERRE,
-            ASIGNACION_IGUAL,
-            PUNTO_Y_COMA,
-            SI,
-            SINO,
-            SINO_SI,
-            MIENTRAS,
-            HACER,
-            DESDE,
-            HASTA,
-            INCREMENTO,
-            BARRAS_COMENTARIO,
-            COMMAS
+            TIPO_OBJETO,
+            IDENTIFICADOR,
+            VALOR_ENTERO,
+            VALOR_DECIMAL,
+            VALOR_CADENA,
+            VALOR_BOOLEAN,
+            VALOR_CHART,
+            OPERADOR_ARITMETICO,
+            OPERADOR_RELACIONAL,
+            OPERADOR_LOGICO,
+            AGRUPACION,
+            DECLARACION,
+            PALABRA_RESERVADA,
+            COMENTARIO,
+            COMMAS,
+            ERROR
         }
 
         private Tipo tipoToken;
         private String valor;
+        int linea;
 
-        public Token(Tipo tipoToken, String valor)
+        public Token(Tipo tipoToken, String valor, int linea)
         {
             this.tipoToken = tipoToken;
             this.valor = valor;
+            this.linea = linea;
         }
 
-        public String getContenido()
+        public String getValor()
         {
             return valor;
         }
@@ -69,85 +48,36 @@ namespace ProyectoLenguaje
         public string getTipo()
         {
             switch(tipoToken){
-                case Tipo.ENTERO:
-                    return "numero entero";                   
-                case Tipo.DECIMAL:
-                    return "decimal";
-                case Tipo.CADENA:
-                    return "cadena";                    
-                case Tipo.BOOLEAN:
-                    return "boolean"; 
-                case Tipo.CHART:
-                    return "chart";
-                case Tipo.IDENTIFICADOR_ENTERO:
-                    return "identificador entero";
-                case Tipo.IDENTIFICADOR_DECIMAL:
-                    return "identificador decimal";
-                case Tipo.IDENTIFICADOR_CADENA:
-                    return "identificador cadena";
-                case Tipo.IDENTIFICADOR_BOOLEAN:
-                case Tipo.IDENTIFICADOR_CHART:
-                    return "identificador chart";
-                case Tipo.OPERADOR_MAS:
-                    return "identificador mas";
-                case Tipo.OPERADOR_MENOS:
-                    return "identificador menos";
-                case Tipo.OPERADOR_MAS_MAS:
-                    return "operador mas mas";
-                case Tipo.OPERADOR_MENOS_MENOS:
-                    return "operador menos menos";
-                case Tipo.OPERADOR_POR:
-                    return "operador por";
-                case Tipo.OPERADOR_SLICE:
-                    return "operador slash";
-                case Tipo.OPERADOR_MAYOR:
-                    return "operador mayor";
-                case Tipo.OPERADOR_MENOR:
-                    return "operador menor";
-                case Tipo.OPERADOR_MAYOR_IGUAL:
-                    return "operador mayor-igual";
-                case Tipo.OPERADOR_MENOR_IGUAL:
-                    return "operador menor-igual";
-                case Tipo.OPERADOR_IGUAL_IGUAL:
-                    return "operador igual-igual";
-                case Tipo.OPERADOR_NO_IGUAL:
-                    return "operadon no igual";
-                case Tipo.OPERADOR_OR:
-                    return "operador";
-                case Tipo.OPERADOR_AND:
-                    return "operador and";
-                case Tipo.OPERADOR_NO:
-                    return "operador no";
-                case Tipo.PARENTESIS_APERTURA:
-                    return "parentesis apertura";
-                case Tipo.PARENTESIS_CIERRE:
-                    return "parentesis cierre";
-                case Tipo.ASIGNACION_IGUAL:
-                    return "asignacion igual";
-                case Tipo.PUNTO_Y_COMA:
-                    return "punto & coma";
-                case Tipo.SI:
-                    return "if";
-                case Tipo.SINO:
-                    return "else";
-                case Tipo.SINO_SI:
-                    return "if else";
-                case Tipo.MIENTRAS:
-                    return "while";
-                case Tipo.HACER:
-                    return "do";
-                case Tipo.DESDE:
-                    return "from";
-                case Tipo.HASTA:
-                    return "hasta";
-                case Tipo.INCREMENTO:
-                    return "incremento";
-                case Tipo.BARRAS_COMENTARIO:
-                    return "barras comentario";
+                case Tipo.TIPO_OBJETO:
+                    return "tipo de objeto";                   
+                case Tipo.IDENTIFICADOR:
+                    return "identificador";
+                case Tipo.VALOR_ENTERO:
+                    return "valor entero";
+                case Tipo.VALOR_DECIMAL:
+                    return "valor decimal";
+                case Tipo.VALOR_CADENA:
+                    return "valor cadena";
+                case Tipo.VALOR_BOOLEAN:
+                    return "valor boolean";
+                case Tipo.VALOR_CHART:
+                    return "valor chart";
+                case Tipo.OPERADOR_ARITMETICO:
+                    return "operador aritmetico";
+                case Tipo.OPERADOR_RELACIONAL:
+                    return "operador relacional";
+                case Tipo.OPERADOR_LOGICO:
+                    return "operador logico";
+                case Tipo.PALABRA_RESERVADA:
+                    return "palabra reservada";
+                case Tipo.AGRUPACION:
+                    return "Agrupacion";
                 case Tipo.COMMAS:
                     return "coma";
+                case Tipo.COMENTARIO:
+                    return "comentario";
                 default:
-                    return "default";
+                    return "ERROR";
             }
         }
     }
